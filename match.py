d3534676f3adbbf3081708450dc3bbdf71cdbf42
Adam Zink <adamzink55@gmail.com>
2017-10-01 10:57:07 -0400

    #2 - Reshape and sum the image data by time and frequency buckets

--- match.py
+++ match.py
@@ -6,28 +6,29 @@ import cv2
 # sox session.wav -n remix 2 rate 6k spectrogram -m -r -X <specWidth> -y <specHeight> -z 80 -o short_song.png
 
 specWidth = 360
-specHeight = 513
-splitSize = 360
+specHeight = 512  # instead of 513 because top row is deleted
 
-image_filename = 'sin_G4.png'
+timeBuckets = 2
+frequencyBuckets = 2
+
+
+image_filename = 'gray.png'
 
 # import spectrogram and format as numpy array
-def get1dSpectrogramSlices(filename, imgWidth, numSlices=1):
-	img = cv2.imread(filename, 0) / 255.0
-	slicesArray = np.split(img, int(imgWidth/splitSize), axis=1)
-	resultArray = np.empty(shape=(0, splitSize*specHeight))
-	sliceCount = 0
-	for slice in slicesArray:
-		slice = slice.flatten()
-		slice = np.expand_dims(slice, axis=0)
-		resultArray = np.append(resultArray, slice, axis=0)
-		sliceCount += 1
-		if (sliceCount >= numSlices):
-			break
-	return resultArray
+def getReducedSpectrogram(filename, imgWidth, imgHeight, timeBuckets, frequencyBuckets):
+	imgFull = cv2.imread(filename, 0) / 255.0
+	
+	# delete top row so images are evenly divisible by 2, 4, etc
+	img = np.delete(imgFull, (0), axis=0)
+	#print(str(img.shape) + ' -> ' + str(img))
+	
+	imgReshape = img.reshape(int(specHeight / frequencyBuckets), frequencyBuckets, int(specWidth / timeBuckets), timeBuckets).sum(axis=3).sum(axis=1)
+	#print(str(imgReshape.shape) + ' -> ' + str(imgReshape))
+	
+	return imgReshape
 
 # Get data for 1 image
-image_data = get1dSpectrogramSlices(os.path.join(os.getcwd(), 'img', 'input', image_filename), specWidth, int(specWidth / splitSize))
+image_data = getReducedSpectrogram(os.path.join(os.getcwd(), 'img', 'input', image_filename), specWidth, specHeight, timeBuckets, frequencyBuckets)
 
 print(str(image_data.shape) + ' -> ' + str(image_data))
 
