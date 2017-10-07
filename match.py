67dc1a9573dde0bae631f2ea61c19f141fbe822e
Adam Zink <adamzink55@gmail.com>
2017-10-07 03:03:29 -0400

    #9 - Convert log scale values in spectrogram to linear scale

--- match.py
+++ match.py
@@ -3,10 +3,22 @@ import numpy as np
 import cv2
 
 # SOX command to generate spectrogram:
-# sox session.wav -n remix 2 rate 6k spectrogram -m -r -X <specWidth> -y <specHeight+1> -z 80 -o short_song.png
+# sox session.wav -n remix 2 rate 6k spectrogram -m -r -X <specWidth> -y <specHeight+1> -z <zAxisDbRange> -o short_song.png
+
+# sox: range 1 - 5000
+xAxisPixelsPerSecond = 360
+
+# sox: power of 2, plus 1
+yAxisPixels = (2**9) + 1
+
+#sox: range 20 - 180 (keep as multiple of 10 for simplicity)
+zMultipleOfTen = 8
+zAxisDbRange = 10 * zMultipleOfTen
+
 
 specWidth = 360
-specHeight = 512  # instead of 513 because top row is deleted
+specHeight = yAxisPixels - 1  # will delete top row of data later so height is evenly divisible by 2
+
 
 timeBuckets = int(specWidth)
 frequencyBuckets = int(specHeight)
@@ -20,28 +32,35 @@ if (frequencyBuckets == 0 or specHeight % frequencyBuckets != 0):
 timeBucketWidth = int(specWidth / timeBuckets)
 frequencyBucketHeight = int(specHeight / frequencyBuckets)
 
-image_filename = 'gray.png'
 
+image_filename = 'light_gray.png'
 
 # import spectrogram and format as numpy array
-def getReducedImage(filename, imgWidth, imgHeight, imgSectionWidth, imgSectionHeight):
-	imgFull = cv2.imread(filename, 0) / 255.0
+def getReducedSpectrogram(filename, imgWidth, imgHeight, imgSectionWidth, imgSectionHeight):
+	# read image as monochrome and normalize to values between 0 and 1
+	imgNormalized = cv2.imread(filename, 0) / 255.0
 	
-	# delete top row so images are evenly divisible by 2, 4, etc
-	img = np.delete(imgFull, (0), axis=0)
-	#print(str(img.shape) + ' -> ' + str(img))
+	# delete top row so image height is evenly divisible by 2, 4, etc
+	imgLog = np.delete(imgNormalized, (0), axis=0)
+	#print(str(imgLog.shape) + ' -> ' + str(imgLog))
+	
+	# convert decibel values to linear scale
+	# use 0 as linear value if spectrogram is black
+	imgLinear = np.where(imgLog == 0, 0, 10.0 ** ((imgLog - 1.0) * zMultipleOfTen))
+	#print(str(imgLinear.shape) + ' -> ' + str(imgLinear))
 	
 	if(imgSectionWidth == 1 and imgSectionHeight == 1):
-		return img
+		return imgLinear
 		
 	else:
-		imgReshape = img.reshape(int(imgHeight / imgSectionHeight), imgSectionHeight, int(imgWidth / imgSectionWidth), imgSectionWidth).mean(axis=3).mean(axis=1)
+		imgReshape = imgLinear.reshape(int(imgHeight / imgSectionHeight), imgSectionHeight, int(imgWidth / imgSectionWidth), imgSectionWidth).mean(axis=3).mean(axis=1)
 		#print(str(imgReshape.shape) + ' -> ' + str(imgReshape))
 
 		return imgReshape
 
+
 # Get data for 1 image
-image_data = getReducedImage(os.path.join(os.getcwd(), 'img', 'input', image_filename), specWidth, specHeight, timeBucketWidth, frequencyBucketHeight)
+image_data = getReducedSpectrogram(os.path.join(os.getcwd(), 'img', 'input', image_filename), specWidth, specHeight, timeBucketWidth, frequencyBucketHeight)
 
 print(str(image_data.shape) + ' -> ' + str(image_data))
 
